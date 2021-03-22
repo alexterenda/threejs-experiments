f8b70ddfb8e3581cc8edcff1893aeb7b54d0ac70
Alex Terenda <alexterenda@gmail.com>
2021-03-22 13:13:32 +0000

    Webpack config

---
+++ 04-webpack/src/script.js
@@ -0,0 +1,33 @@
+import "./style.css";
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+  width: 600,
+  height: 450,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+
+scene.add(camera);
+
+const canvas = document.getElementById("webgl");
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
