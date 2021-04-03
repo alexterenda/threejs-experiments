9fcc1899a0af20e5fa6be88c5daa26016921fcbd
Alex Terenda <alexterenda@gmail.com>
2021-04-03 16:16:25 +0100

    Update commit

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -23,7 +23,7 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 5;
 scene.add(camera);
 
 // Renderer
